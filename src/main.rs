2d3b30c644299485e473503049243e7630a83cb7
Ignytis <155588001+ignytis@users.noreply.github.com>
2023-12-19 23:44:32 +0200

    Basic HTTP server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use actix_web::{post, App, HttpRequest, HttpServer, Result};
+use log::info;
+
+
+#[post("/{topic}")]
+async fn index(req: HttpRequest) -> Result<String> {
+    // TODO: write to Kafka topic
+    //let topic: String = req.match_info().get("topic").unwrap().parse().unwrap();
+    Ok(String::from(""))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::init();
+
+    let host =  env::var("SEND2KAFKA_HTTP_HOST").unwrap_or(String::from("127.0.0.1"));
+    let port = env::var("SEND2KAFKA_HTTP_PORT").unwrap_or(String::from("8080")).parse::<u16>().expect("Incorrect HTTP port provided");
+    info!("Listening on {}:{}", host, port);
+
+    HttpServer::new(|| {
+        App::new().service(index)
+    })
+    .bind((host, port))?
+    .run()
+    .await
 }

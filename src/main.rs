8e916ece65bdbd73f8ad1b3c06b05635bbe41eb2
Ignytis <155588001+ignytis@users.noreply.github.com>
2023-12-22 22:26:04 +0200

    Configurable config file location

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ mod configuration;
 mod http_api;
 mod kafka_producer;
 
+use std::env;
 use config::Config as LibConfig;
-
 use crate::configuration::Config;
 
 
@@ -12,12 +12,13 @@ use crate::configuration::Config;
 async fn main() -> std::io::Result<()> {
     env_logger::init();
 
+    let cfg_file = env::var("SEND2KAFKA__CONFIG").unwrap_or(String::from("config.yaml"));
     let cfg = LibConfig::builder()
         .set_default("http.host", "127.0.0.1").unwrap()
         .set_default("http.port", "8080").unwrap()
         .set_default("kafka.bootstrap_servers", "localhost:9092").unwrap()
         .add_source(config::Environment::with_prefix("SEND2KAFKA").separator("__"))
-        .add_source(config::File::with_name("config.yaml").required(false))
+        .add_source(config::File::with_name(cfg_file.as_str()).required(false))
         .build()
         .unwrap();
 

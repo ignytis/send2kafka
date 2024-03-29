20999cd0fb2952887a558df9cd0d451be0a497c1
Ignytis <155588001+ignytis@users.noreply.github.com>
2024-03-29 22:08:49 +0200

    A healthcheck endpoint

--- src/kafka_producer.rs
+++ src/kafka_producer.rs
@@ -12,16 +12,18 @@ pub struct KafkaProducer {
 
 impl KafkaProducer {
     pub fn new(cfg: &HashMap<String, String>) -> Self {
-        let mut rd_producer = &mut ClientConfig::new();
+        let mut kafka_config = &mut ClientConfig::new();
         for (k, v) in cfg.iter() {
-            rd_producer = rd_producer.set(k.replace("_", "."), v);   
+            kafka_config = kafka_config.set(k.replace("_", "."), v);
         }
-        let rd_producer = rd_producer.create().expect("Can't create a Kafka producer");
+        let rd_producer = kafka_config.create().expect("Can't create a Kafka producer");
+        let producer = KafkaProducer { rd_producer };
+
         let bootstrap_servers = cfg.get("bootstrap_servers")
             .unwrap_or(&String::from("(not provided)"))
             .clone();  // to resolve a '&String vs String' issue
-        let producer = KafkaProducer { rd_producer };
         info!("Connected to Kafka. Bootstrap servers: {}", bootstrap_servers);
+
         producer
     }
 
@@ -29,13 +31,14 @@ impl KafkaProducer {
     // On failure returns an error message
     pub async fn produce(&self, topic_name: &str, key: &Option<String>, payload: Vec<u8>) -> Result<(i32, i64), String> {
         let p = &payload.to_vec();
-        let mut future = FutureRecord::to(topic_name) .payload(p);
-        future = match key {
-            Some(k) => future.key(k),
-            None => future,
+        let mut future_record = FutureRecord::to(topic_name).payload(p);
+        future_record = match key {
+            Some(k) => future_record.key(k),
+            None => future_record,
         };
         let res = self.rd_producer
-            .send(future, Duration::from_secs(0)).await;
+            .send(future_record, Duration::from_secs(0)).await;
+
         match res {
             Ok(d) => Ok(d),
             Err((kafka_error, _)) => Err(format!("{:?}", kafka_error))

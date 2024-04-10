6d1ce821a4910cce44c7f0c87ef903990b1bb053
Ignytis <155588001+ignytis@users.noreply.github.com>
2024-04-10 22:22:30 +0300

    Removed a status which is not in use

--- src/app_state.rs
+++ src/app_state.rs
@@ -1,19 +1,14 @@
 use crate::kafka_producer::KafkaProducer;
 
 #[derive(Clone)]
-pub enum Status {
-    Initializing,
-    Ok,
-}
-
-#[derive(Clone)]
 pub struct AppState {
     pub producer: KafkaProducer,
-    pub status: Status,
 }
 
 impl AppState {
     pub fn new(producer: KafkaProducer) -> Self {
-        AppState{producer, status: Status::Initializing}
+        AppState {
+            producer
+        }
     }
 }
--- src/http_api/mod.rs
+++ src/http_api/mod.rs
@@ -3,7 +3,7 @@ pub mod endpoints;
 use actix_web::{App, HttpServer, web::Data};
 use log::info;
 
-use crate::app_state::{AppState, Status};
+use crate::app_state::AppState;
 use crate::kafka_producer::KafkaProducer;
 use crate::configuration::Config;
 
@@ -16,8 +16,7 @@ pub async fn start(cfg: Config) -> std::io::Result<()> {
         // Should we have 1 producer instead? Should we pass Arc<Mutex<Producer>>?
         // Will it block HTTP handlers? Perhaps current approach is still better
         let producer = KafkaProducer::new(&cfg.kafka);
-        let mut state = AppState::new(producer);
-        state.status = Status::Ok; // FIXME: this has no effect. Need to catch conneciton issues other way
+        let state = AppState::new(producer);
 
         App::new()
             .app_data(Data::new(state))
